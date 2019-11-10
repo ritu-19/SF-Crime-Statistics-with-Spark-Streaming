44ac345f0fb9ac06f8a3801cd1e3c14760875865
Ritu Yadav <38189524+ritu-19@users.noreply.github.com>
2019-11-11 02:18:45 +0530

    Producer Code

---
+++ producer_server.py
@@ -0,0 +1,44 @@
+import pathlib
+import json
+import logging
+import pykafka
+import time
+
+INPUT_FILE = 'police-department-calls-for-service.json'
+
+logger = logging.getLogger(__name__)
+
+
+def read_file() -> json:
+    with open(INPUT_FILE, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def generate_data() -> None:
+    data = read_file()
+    for i in data:
+        message = dict_to_binary(i)
+        producer.produce(message)
+        time.sleep(1)
+        print(i)
+
+
+# TODO complete this function
+def dict_to_binary(json_dict: dict) -> bytes:
+    """
+    Encode your json to utf-8
+    :param json_dict:
+    :return:
+    """
+    return json.dumps(json_dict).encode("utf-8")  
+
+# TODO set up kafka client
+if __name__ == "__main__":
+    #client =
+    client = pykafka.KafkaClient(hosts="127.0.0.1:9092")
+    print("topics", client.topics)
+    producer = client.topics[b'demo'].get_producer()
+
+    generate_data()
+
